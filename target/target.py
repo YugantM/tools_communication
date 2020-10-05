fab2b7f0d66b60c6872dede7313dfc8178b41237
Yugant <hadiyalyugant@gmail.com>
2020-10-05 17:36:57 +0200

    updates in loading of json

--- target/target.py
+++ target/target.py
@@ -6,7 +6,7 @@ def target(key):
 
     print("from the traget function","".join(sys.stdin))
     #string = json.dumps(sys.stdin)
-    string = json.load(sys.stdin)
+    string = json.loads(sys.stdin)
 
     if key in string.keys():
         print(string[key])
